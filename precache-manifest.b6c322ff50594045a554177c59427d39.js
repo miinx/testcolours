854539531d9aa8f2247cd58bdac63a33f458691d
Karen Bowen <miinx@users.noreply.github.com>
2020-01-06 11:43:42 +1100

    Updates

---
+++ precache-manifest.b6c322ff50594045a554177c59427d39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89fbfc72200c70894b7def3e30251259",
+    "url": "/testnavpagination/index.html"
+  },
+  {
+    "revision": "ca4441f05c88339e205f",
+    "url": "/testnavpagination/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "343a71a92facc410bf62",
+    "url": "/testnavpagination/static/js/2.58ac7251.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/testnavpagination/static/js/2.58ac7251.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ca4441f05c88339e205f",
+    "url": "/testnavpagination/static/js/main.6617e2f3.chunk.js"
+  },
+  {
+    "revision": "85cc3c6a46b85c2782b2",
+    "url": "/testnavpagination/static/js/runtime-main.9c7aa884.js"
+  }
+]);
